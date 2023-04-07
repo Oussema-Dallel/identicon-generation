7e50177841f163a2c16accca50bfb75cf3c333f5
Oussema Dallel <oussemadallel@gmail.com>
2023-04-07 17:17:57 +0200

    add image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,15 +4,18 @@ defmodule Identicon do
   @moduledoc """
   Documentation for `Identicon`.
   """
-  @spec! main(String.t()) :: list
+  @spec! main(String.t()) :: struct()
   def main(input) do
     input
     |> hash_input
   end
 
-  @spec! hash_input(String.t()) :: list
+  @spec! hash_input(String.t()) :: struct()
   def(hash_input(input)) do
-    :crypto.hash(:md5, input)
-    |> :binary.bin_to_list()
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
   end
 end
